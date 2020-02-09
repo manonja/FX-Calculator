a319232ad7678a02f2ebbd4abc7bbaf4b04d0c72
Manon <manon.jacquin@gmail.com>
2020-02-09 16:50:09 +0000

    Import ResultField

--- fx-calculator/src/components/pages/SPA/index.tsx
+++ fx-calculator/src/components/pages/SPA/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../../../App.css';
 import { Header }  from '../../atoms/Header'
-import FXForm from '../../organism/form'
+import FXForm from '../../organism/FXForm'
+import ResultField from '../../atoms/ResultField'
+
+
 
 const SPA = () => {
   return (
       <>
         <Header title="FX-Calculator"/>
-        <FXForm  onSubmit={() => {}}></FXForm>
+        <FXForm  onSubmit={({base, target, amount}) => {console.log(base, target, amount)}}></FXForm>
+        {/* <ResultField result={result}/> */}
    </>
   );
 }
