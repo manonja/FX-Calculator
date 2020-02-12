fd5476038cbd495137a761c05ec600de6aec597f
Manon <manon.jacquin@gmail.com>
2020-02-12 21:33:05 +0000

    Adds link where to fetch

--- fx-calculator/src/api/index.tsx
+++ fx-calculator/src/api/index.tsx
@@ -1,7 +1,13 @@
-export const getCcyExchangeRate = () => {
+const API_KEY = 'NTOQMSUKNQT0Y6ZW';
+
+
+export const getExchangeRate = (func: void, fromCcy: string, toCcy: string) => {
     console.log('CURRENCY_EXCHANGE_RATE')
+    // TODO: fetch data
+    return `https://www.alphavantage.co/query?function=${func}&from_currency=${fromCcy}&to_currency=${toCcy}&apikey=${API_KEY}`;
 }
 
-export const getFXDaily = () => {
+export const getFXDaily = (func: void, fromSymbol: string, toSymbol: string ) => {
     console.log('FX_DAILY')
+    return `https://www.alphavantage.co/query?function=${func}&from_symbol=${fromSymbol}&to_symbol=${toSymbol}&apikey=${API_KEY}`;
 }
