763329e9cb50010265dee8d9cdb2afd79e7368fc
Manon <manon.jacquin@gmail.com>
2020-02-15 14:15:35 +0000

    Fetch url works

--- fx-calculator/src/api/index.tsx
+++ fx-calculator/src/api/index.tsx
@@ -1,13 +1,14 @@
 const API_KEY: string = 'NTOQMSUKNQT0Y6ZW';
 
-
-export const getExchangeRate = (func: void, fromCcy: string, toCcy: string) => {
-    console.log('CURRENCY_EXCHANGE_RATE')
-    // TODO: fetch data
-    return `https://www.alphavantage.co/query?function=${func}&from_ccy=${fromCcy}&to_ccy=${toCcy}&apikey=${API_KEY}`;
+export const constructAlphaVantageUrl = (func: string, func_call: string): string => {
+    return `https://www.alphavantage.co/query?function=${func}&${func_call}&apikey=${API_KEY}`;
 }
 
-export const getFXDaily = (func: void, fromSymbol: string, toSymbol: string ) => {
-    console.log('FX_DAILY')
-    return `https://www.alphavantage.co/query?function=${func}&from_symbol=${fromSymbol}&to_symbol=${toSymbol}&apikey=${API_KEY}`;
+export const getFXUrl = (fromCcy: string, toCcy: string): string => {
+    return constructAlphaVantageUrl('CURRENCY_EXCHANGE_RATE', `from_currency=${fromCcy}&to_currency=${toCcy}`)
 }
+
+export const getFXDailyUrl = (fromCcy: string, toCcy: string): string => {
+    return constructAlphaVantageUrl('CURRENCY_EXCHANGE_RATE', `from_symbol=${fromCcy}&to_symbol=${toCcy}`)
+}
+
--- fx-calculator/src/components/pages/SPA/index.tsx
+++ fx-calculator/src/components/pages/SPA/index.tsx
@@ -4,7 +4,7 @@ import { Header }  from '../../atoms/Header'
 import FXForm from '../../organism/FXForm'
 import ResultField from '../../atoms/ResultField'
 import { AppState, Action, initialFXState, reducer, ActionType } from '../../../context'
-
+import {getFXUrl} from '../../../api'
 
 const SPA = () => {
   const [ state, dispatch ] = React.useReducer<React.Reducer<AppState, Action>>(reducer, initialFXState);
@@ -14,6 +14,14 @@ const SPA = () => {
         <Header title="FX-Calculator"/>
         <FXForm  state={initialFXState} onSubmit={({from_ccy, to_ccy, amount}) => {
           dispatch({type: ActionType.UPDATE_FORM, payload: { from_ccy, to_ccy, amount }});
+          // TODO: refactor fetch
+          fetch(getFXUrl(from_ccy, to_ccy))
+          .then(res => res.json())
+          .then((data) => {
+            const fx_rate: number = parseFloat(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+            dispatch({type: ActionType.UPDATE_FX_RATE, payload: {fx_rate}})
+          })
+
           // console.log(from_ccy, to_ccy, amount)
           console.log(state)
           }}>
